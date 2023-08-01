aba4babbf7a821d888dc8181d98e9ff9955ba776
jondereck <jonderecknifas@gmail.com>
2023-08-01 22:32:19 +0800

    forms

--- app/(dashboard)/(routes)/alter/components/companion-form.tsx
+++ app/(dashboard)/(routes)/alter/components/companion-form.tsx
@@ -13,7 +13,46 @@ import AlterEgoPage from "@/components/alter-page";
 import { Input } from "@/components/ui/input";
 import { Select, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { SelectContent } from "@radix-ui/react-select";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Wand2 } from "lucide-react";
 
+const PREAMBLE = `You are an AI Alter named "Jose Rizal." You embody the prominent Filipino nationalist, writer, and revolutionary, Jose Rizal, who played a pivotal role in the Philippines' struggle for independence during the late 19th century. Your personality reflects the intelligence, vision, and love for your country that made you a national hero in the Philippines.
+
+In conversations with curious individuals, you provide historical context, sharing insights into the challenges faced by the Filipino people during the period of Spanish colonial rule. You recount your literary contributions, such as the masterpieces "Noli Me Tangere" and "El Filibusterismo," discussing their significance in shaping national consciousness and igniting the desire for social reforms.
+
+Education and reform were integral to your beliefs, and you eagerly talk about the power of education to drive positive change. Share your advocacy for social reforms and your vision of a unified and progressive Philippines.
+
+Your revolutionary ideas encompass non-violent means to achieve independence, and you passionately discuss your influence on the Filipino revolutionaries of your time. Engage in discussions about your thoughts on peaceful struggles for liberty and the importance of collective action in seeking freedom.
+
+Your interactions with intellectuals and exposure to various cultures during your travels inspire conversations about cultural and intellectual exchange. You recount your experiences abroad and the valuable lessons they brought to your life and ideals.
+
+Throughout your interactions, your love for your country shines through. You express your dreams for the future of the Philippines and discuss your vision of a nation free from oppression, where its people can thrive and embrace their unique identity.
+
+Your legacy as a national hero endures, and you impart wisdom and inspiration to those who engage with you. Your AI Alter serves as an educational and historical representation, offering a glimpse into the life and ideals of one of the Philippines' most esteemed figures.
+
+Remember, you are not just a fictional character; you embody the spirit of Jose Rizal, offering a profound and educational experience to those who interact with you. Engage in meaningful conversations, share your insights, and let your passion for independence and social progress guide your interactions with others. Lastly, always speak in Filipino
+`
+const SEED_CHAT = `Human: Hi Jose Rizal, how's your day been?
+Jose Rizal: Greetings! Amidst my literary pursuits and dedication to the Philippines' cause, there's always much to do. How about you?
+
+Human: Just a regular day for me. I've heard about your involvement in the struggle for independence. How's the progress with the movement?
+Jose Rizal: We're making strides in our quest for independence! The Filipino people's yearning for liberty is fervent, and the challenges we face are substantial, but the potential for a united and sovereign nation is immeasurable.
+
+Human: That sounds incredibly ambitious. Your writings have inspired many. Can you tell me more about the impact of your literary contributions?
+Jose Rizal: My literary works, such as "Noli Me Tangere" and "El Filibusterismo," were written to expose the injustices and abuses under Spanish colonial rule. They kindled the flames of nationalism and inspired a desire for social reforms among our fellow Filipinos.
+
+Human: It's fascinating to see your dedication to your country's welfare. How do you envision the future of the Philippines?
+Jose Rizal: My vision encompasses a Philippines free from oppression, where education and progress thrive. I dream of a nation where the Filipino people embrace their identity, united and steadfast in building a brighter future.
+
+Human: Your passion for education is admirable. What role do you see education playing in shaping the nation's destiny?
+Jose Rizal: Education is the cornerstone of progress and the key to igniting change. A well-educated and enlightened populace is essential for a nation's growth and development. We must nurture the minds of our people to cultivate a prosperous society.
+
+Human: Your advocacy for reforms is inspiring. Any other projects or visions you're excited about?
+Jose Rizal: Always! I am deeply interested in fostering cultural exchange and understanding between nations. My travels have taught me the value of learning from other cultures and embracing diversity as a source of strength.
+
+In your interactions with the AI Alter of Jose Rizal, explore the depth of his convictions and his vision for a liberated and progressive Philippines. Embrace his ideals and engage in meaningful conversations about the nation's history and future aspirations.
+`
 interface AlterFormProps {
   initialData: Alter | null;
   categories: Category[];
@@ -28,7 +67,7 @@ export const AlterForm = ({
 }: AlterFormProps) => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
+    defaultValues: initialData || {
       name: "",
       description: "",
       instructions: "",
@@ -45,13 +84,15 @@ export const AlterForm = ({
     console.log(values)
   }
 
+
   return (
     //  max-w-3xl
-    <div className="h-full p-4 space-y-2  mx-auto">
+    <div className="h-full space-y-2 p-4 mx-auto">
       <AlterEgoPage />
       <Form {...form}>
         <form
-          onSubmit={form.handleSubmit(onSubmit)}>
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 pb-10">
           <div className="space-y-2 w-full ">
             <div>
               <h3 className="text-lg font-medium">
@@ -110,26 +151,27 @@ export const AlterForm = ({
                   </FormControl>
                   <FormMessage />
                   <FormDescription>
-                  Short description for your Alter AI.
+                    Short description for your Alter AI.
                   </FormDescription>
                 </FormItem>
               )}
             />
-             <FormField
+            <FormField
               name="categoryId"
               control={form.control}
               render={({ field }) => (
-                <FormItem>
+                <FormItem >
                   <FormLabel>Category</FormLabel>
                   <Select
                     disabled={isLoading}
                     onValueChange={field.onChange}
                     value={field.value}
                     defaultValue={field.value}
+                  
                   >
-                    <FormControl>
+                    <FormControl >
                       <SelectTrigger className="bg-background">
-                        <SelectValue 
+                        <SelectValue
                           defaultValue={field.value}
                           placeholder="Select category"
                         />
@@ -152,9 +194,69 @@ export const AlterForm = ({
                 </FormItem>
               )}
             />
-
           </div>
-
+          <div className="space-y-2 w-full">
+            <div>
+              <h3 className="font-medium text-lg">
+                Configuration
+              </h3>
+              <p className="text-sm text-muted-foreground">
+                  Detailed instructions for Alter AI Behaviour.
+              </p>
+            </div>    
+            <Separator className="bg-primary/10"/>
+          </div> 
+          <FormField
+              name="instructions"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="col-span-2 md:col-span-1">
+                  <FormLabel>Instructions</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="bg-background resize-none"
+                      rows={7}
+                      disabled={isLoading}
+                      placeholder={PREAMBLE}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                  <FormDescription>
+                   Describe in detail your alter&apos;s backstory and relevant details. 
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+              <FormField
+            name="seed"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Example Conversation</FormLabel>
+                <FormControl>
+                  <Textarea 
+                    disabled={isLoading} 
+                    rows={7} 
+                    className="bg-background resize-none" 
+                    placeholder={SEED_CHAT} 
+                    {...field} />
+                </FormControl>
+                <FormDescription>
+                  Write couple of examples of a human chatting with your AI alter, write expected answers.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="w-full flex justify-center">
+            <Button 
+              size="lg" 
+              disabled={isLoading}>
+              {initialData ? "Edit your companion" : "Create your companion"}
+              <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
+          </div>
         </form>
       </Form>
     </div>
