48bda82de358ea7fbdad62c1996cb0f40a245228
jondereck <jonderecknifas@gmail.com>
2023-08-03 22:57:17 +0800

    fix-sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -122,74 +122,86 @@ const Sidebar = ({
 
   return (
     <>
-      {/* "Open Sidebar" button section */}
-      {isLargeScreen && !useClientOpen && (
-        <button onClick={handleUseClientClick}>
-          <div className="fixed top-4 left-4 p-3 rounded-md text-white ">
-            <BsLayoutSidebarInsetReverse size={25}/>
-          </div>
-        </button>
-      )}
-
-
-      {/* Render the "Use Client" content based on state */}
-      {isLargeScreen && useClientOpen && (
-        <div className=" py-4 flex flex-col h-full
-      bg-[#111827] text-white">
-          <div className="px-3 py-2 flex-1">
-            <Link href="/dashboard" className="flex items-center 
-          pl-3 mb-14">
-              <div className="relative w-10 h-10 mr-4">
-                <Image
-                  fill
-                  alt="Logo"
-                  src="/nlogo.png"
-                />
-              </div>
-              <h1 className={cn("text-2xl font-bold", font.className)} >
-                GenioAi
-              </h1>
-              <div className="pl-4">
-                <Button
-                variant="ghost"
-                size="icon"
-                onClick={handleUseClientClick}
-              >
-                <BsLayoutSidebarInset size={25} />
-              </Button>
-              </div>
-              
-            </Link>
-
-
-            <div className="space-y-1">
-              {routes.map((route) => (
-                <Link
-                  key={route.href}
-                  onClick={handleLinkClick}
-                  href={route.href}
-                  className={cn(
-                    "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
-                    pathname === route.href ? "text-white bg-white/10" : "text-zinc-400",
-                  )}
-                >
-                  <div className="flex items-center flex-1">
-                    <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
-                    {route.label}
+      {isLargeScreen && (
+        <div className={`flex h-screen ${!useClientOpen ? "w-0": "w-72"  }`}>
+          {/* Main Content */}
+          {isLargeScreen && (
+            <div
+              className={`py-4 flex flex-col h-full bg-[#111827] text-white transform ${useClientOpen ? 'translate-x-0' : '-translate-x-[250px]'
+                } transition duration-310`}
+            >
+              <div className="px-4 py-2 flex-1">
+               <div className="flex items-start">
+                <Link href="/dashboard" className="flex items-center 
+                 pl-3 mb-14">
+                  <div className="relative w-10 h-10 mr-4">
+                    <Image
+                      fill
+                      alt="Logo"
+                      src="/nlogo.png"
+                    />
                   </div>
+                  <h1 className={cn("text-2xl font-bold", font.className)} >
+                    GenioAi
+                  </h1>
+                  
                 </Link>
-              ))}
+                <div className="pl-4">
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      onClick={handleUseClientClick}
+                    >
+                      <BsLayoutSidebarInset size={25} />
+                    </Button>
+                  </div>
+                  </div>
+
+                <div className="space-y-1">
+                  {routes.map((route) => (
+                    <Link
+                      key={route.href}
+                      onClick={handleLinkClick}
+                      href={route.href}
+                      className={cn(
+                        "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                        pathname === route.href ? "text-white bg-white/10" : "text-zinc-400",
+                      )}
+                    >
+                      <div className="flex items-center flex-1">
+                        <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                        {route.label}
+                      </div>
+                    </Link>
+                  ))}
+                </div>
+              </div>
+
+              <FreeCounter
+                apiLimitCount={apiLimitCount}
+                isPro={isPro} />
+
             </div>
+          )}
+
+
+          <div className={`flex flex-col flex-1 ${useClientOpen ? 'ml-[200px]' : 'ml-0'}`}>
+            {/* "Open Sidebar" button section */}
+            {isLargeScreen && !useClientOpen && (
+              <button onClick={handleUseClientClick}>
+                <div className="fixed top-4 left-4 p-3 rounded-md bg-slate-500 text-white">
+                  <BsLayoutSidebarInsetReverse size={25} />
+                </div>
+              </button>
+            )}
           </div>
 
-          <FreeCounter
-            apiLimitCount={apiLimitCount}
-            isPro={isPro} />
+
+
+
 
         </div>
       )}
-
-
     </>
   );
 };
